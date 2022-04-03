eaf0aceabfc1b795a82a9c2e5f2054da91a0e236
jarod-f <forbesjarod@gmail.com>
2022-04-03 01:23:38 -0700

    Complete anecdotes exercise

--- part1/react/src/App.js
+++ part1/react/src/App.js
@@ -1,10 +1,16 @@
-import { useState } from "react";
-
+import { useState } from 'react'
 
 /******************************************************************************
  *                          App Code Local Functions                          *
  ******************************************************************************/
-const Header = ({ text }) => <h1>{text}</h1>
+const Heading = ({ text }) => <h1>{text}</h1>
+
+const DisplayAnecdoteInfo = ({ anecdote, votes }) => (
+  <div>
+    <p>{anecdote}</p>
+    <p>has {votes} votes</p>
+  </div>
+)
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
@@ -12,54 +18,59 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
-const StatisticRow = ({ statistic, value }) => (
-  <tr>
-    <td>{statistic}</td>
-    <td>{value}</td>
-  </tr>
-)
-
-const Statistics = ({ good, neutral, bad }) => {
-  const totalClicks = good + neutral + bad;
-
-  // Handle case where no clicks provided
-  if (totalClicks === 0) {
-    return <div>No feedback given</div>;
-  }
-
-  return (
-    <table>
-      <tbody>
-        <StatisticRow statistic='Good' value={good} />
-        <StatisticRow statistic='Neutral' value={neutral} />
-        <StatisticRow statistic='Bad' value={bad} />
-        <StatisticRow statistic='All' value={totalClicks} />
-        <StatisticRow statistic='Average' value={(good - bad) / totalClicks} />
-        <StatisticRow statistic='Positive'
-                      value={`${good / totalClicks * 100} %`} />
-      </tbody>
-    </table>
-  );
+/**
+ * Get a random integer between a min and max value
+ * @param {number} min Inclusive minimum for random value
+ * @param {number} max Exclusive maximum for random value
+ * @returns {number} Random integer within range
+ * @source mdn Math/random docs
+ */
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max-min) + min);
 }
 
 /******************************************************************************
  *                               App Definition                               *
  ******************************************************************************/
 const App = () => {
-  // Save the clicks of each button to their own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  // Anecdotes & anecdote state variables
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ];
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+  const [selected, setSelected] = useState(0);
+
+  // Anecdote function helpers
+  const addVote = () => {
+    let newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  }
+  const setRandomAnecdote = () => setSelected(getRandomInt(0, anecdotes.length));
+
+  // Calculating index of highest voted anecdote
+  // - Can speed up by writing function that only makes one pass of votes array
+  const maxVotesIndex = votes.indexOf(Math.max(...votes));
 
   return (
     <div>
-      <Header text='Provide Feedback' />
-      <Button handleClick={() => setGood(good + 1)} text='Good' />
-      <Button handleClick={() => setNeutral(neutral + 1)} text='Neutral' />
-      <Button handleClick={() => setBad(bad + 1)} text='Bad' />
+      <Heading text='Anecdote of the day' />
+      <DisplayAnecdoteInfo anecdote={anecdotes[selected]}
+                           votes={votes[selected]} />
+      <Button handleClick={addVote} text='vote' />
+      <Button handleClick={setRandomAnecdote} text='Next Anecdote' />
 
-      <Header text='User Statistics' />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Heading text='Anecdote with most votes' />
+      <DisplayAnecdoteInfo anecdote={anecdotes[maxVotesIndex]}
+                           votes={votes[maxVotesIndex]} />
     </div>
   );
 }
